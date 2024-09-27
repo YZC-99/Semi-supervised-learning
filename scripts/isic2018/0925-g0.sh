97503c6082414e8d6b6a840dbb3534227b3b2d8e
yzc <yzc@qq.com>
2024-09-27 20:08:34 +0800

    add hyperplusfixmatch.py

--- scripts/isic2018/0925-g0.sh
+++ scripts/isic2018/0925-g0.sh
@@ -7,13 +7,13 @@ python oct_ssl.py --algorithm 'fullysupervised' --num_train_iter 3400 --device 0
 --all_train_count 7010  --num_eval_iter 3400
 
 
-python oct_ssl.py --algorithm 'hyperplusfixmatch' --num_train_iter 10000 --device 0 --num_labels_mode 'ratio' --num_labels_ratio 0.05 \
+python oct_ssl.py --algorithm 'hyperplusfixmatch' --num_train_iter 15000 --device 0 --num_labels_mode 'ratio' --num_labels_ratio 0.05 \
 --net 'densenet121' --batch_size 64 --num_classes 7 --dataset 'isic2018' --loss 'ce' --save_dir 'isic2018-exp' \
 --all_train_count 7010 --clinical 'sex' --other 'KL+KL+supcon' --num_eval_iter 110 \
 --model_ckpt "/dk1/isic2018-exp/densenet121_/fullysupervised_nlratio0.05__Adam_lr0.001_num_train_iter3400_bs64_seed42/latest_model.pth"
 
 
-python oct_ssl.py --algorithm 'hyperplusfixmatch' --num_train_iter 10000 --device 1 --num_labels_mode 'ratio' --num_labels_ratio 0.05 \
+python oct_ssl.py --algorithm 'hyperplusfixmatch' --num_train_iter 15000 --device 1 --num_labels_mode 'ratio' --num_labels_ratio 0.05 \
 --net 'densenet121' --batch_size 64 --num_classes 7 --dataset 'isic2018' --loss 'ce' --save_dir 'isic2018-exp' \
 --all_train_count 7010 --clinical 'lesion_id' --other 'KL+KL+supcon' --num_eval_iter 110 \
 --model_ckpt "/dk1/isic2018-exp/densenet121_/fullysupervised_nlratio0.05__Adam_lr0.001_num_train_iter3400_bs64_seed42/latest_model.pth"
